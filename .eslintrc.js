c74367a425d98e58329b4f43c8d4ba1c80d6ee0c
Sanchit <Sanchit@gmail.com>
2024-03-07 20:08:30 +0530

    Updated ESLint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@next/next/recommended",
-    "airbnb"
-  ],
-
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -21,7 +14,7 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/no-unescaped-entities": 0,
-    "react/no-danger": 0,
+    "eslintreact/no-danger": 0,
     "react/jsx-max-props-per-line": 0,
     "react/jsx-first-prop-new-line": 0,
     "no-console": 0,
@@ -75,10 +68,5 @@ module.exports = {
         aspects: ["noHref", "invalidHref", "preferButton"]
       }
     ]
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
   }
 };
